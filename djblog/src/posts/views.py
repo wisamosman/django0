e483e5930995c050f297de1732f853e9a2cc5557
wisamosman <viesam_1987@hotmail.com>
2023-06-16 00:56:28 +0200

    add db

--- djblog/src/posts/views.py
+++ djblog/src/posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
+def post_list(request):
+    data = Post.objects.all()
+    return render(request,'posts/posts.html',{})
 # Create your views here.
--- djblog/src/project/urls.py
+++ djblog/src/project/urls.py
@@ -19,8 +19,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from posts.views import post_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/' , post_list),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
