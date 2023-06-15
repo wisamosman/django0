cc19c05749a32910614badfda5d11e68b746f8b1
wisamosman <viesam_1987@hotmail.com>
2023-06-16 00:42:04 +0200

    add db

--- djblog/src/posts/admin.py
+++ djblog/src/posts/admin.py
@@ -4,6 +4,7 @@ from .models import Post , Author
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publish_date']
     list_filter = ['author', 'publish_date', 'tags']
+    search_fields = ['title', 'content']
 
 
 admin.site.register(Post,PostAdmin)
