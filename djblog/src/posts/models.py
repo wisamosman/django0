c591179e5ee44ff21db7ac814a1ecbfe36639982
wisamosman <viesam_1987@hotmail.com>
2023-06-15 23:57:53 +0200

    add db

--- djblog/src/posts/models.py
+++ djblog/src/posts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(max_length=500)
+    
     def __str__(self):
         return self.name
 
