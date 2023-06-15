7eff9123ab8e9cb6e9984ac89fc940160eff0dc6
wisamosman <viesam_1987@hotmail.com>
2023-06-15 23:04:30 +0200

    add db

--- djblog/src/posts/models.py
+++ djblog/src/posts/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=500)
+
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     publish_date = models.DateTimeField()
