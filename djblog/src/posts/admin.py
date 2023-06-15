32096a0d3741ecdd003766a4133784c230dcb794
wisamosman <viesam_1987@hotmail.com>
2023-06-16 00:35:45 +0200

    add db

--- djblog/src/posts/admin.py
+++ djblog/src/posts/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Post , Author
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','publish_date']
+
+    
+admin.site.register(Post,PostAdmin)
 admin.site.register(Author)
 
 # Register your models here.
+
+
+
