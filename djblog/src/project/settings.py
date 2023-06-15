d9fe44803f1ddb40073801f7ce1a575c90f418ff
wisamosman <viesam_1987@hotmail.com>
2023-06-16 00:10:16 +0200

    add db

--- djblog/src/project/settings.py
+++ djblog/src/project/settings.py
@@ -122,8 +122,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     
 ]
+
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
