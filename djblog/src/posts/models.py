17383dbe390d2258b3cbbed18efd208d9e394ca4
wisamosman <viesam_1987@hotmail.com>
2023-06-15 23:52:23 +0200

    add db

--- djblog/src/posts/models.py
+++ djblog/src/posts/models.py
@@ -13,4 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     publish_date = models.DateTimeField()
     content = models.TextField(max_length=15000)
-    Author = models.ForeignKey(Author,related_name='post_author',on_delete=models.CASCADE)
+    author = models.ForeignKey(Author,related_name='post_author',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
